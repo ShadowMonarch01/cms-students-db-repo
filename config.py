60b668a949bc2c14e968f15c1ebb361776b44da0
Trust <gunngptrust@gmail.com>
2021-10-29 10:09:31 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class ApplicationConfig:
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql5447520:nmy2VMhGQW@sql5.freesqldatabase.com/sql5447520'
+
