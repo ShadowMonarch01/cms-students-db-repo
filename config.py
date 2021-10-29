51b75ee1a208fe29d3edfc407cb39627b27b5912
Trust <gunngptrust@gmail.com>
2021-10-29 14:44:29 +0100

    Procfile

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class ApplicationConfig:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql5447520:nmy2VMhGQW@sql5.freesqldatabase.com/sql5447520'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b0a6ed2655bb80:bc712c46@us-cdbr-east-04.cleardb.com/heroku_8df0a1802eec68f'
 
